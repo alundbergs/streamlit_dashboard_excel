6c15889c5ef64087c1a00bc1785126dad18bf602
alundbergs <59663385+alundbergs@users.noreply.github.com>
2023-06-20 15:22:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,21 +3,7 @@ import pandas as pd  # pip install pandas openpyxl
 import plotly.express as px  # pip install plotly-express
 import streamlit as st  # pip install streamlit
 import streamlit.components.v1 as components
-#NEWCODE
-import numpy as np
-import plotly.figure_factory as ff
 
-x1 = np.random.randn(200) - 2
-x2 = np.random.randn(200)
-x3 = np.random.randn(200) + 2
-
-hist_data = [x1, x2, x3]
-group_labels = ['Group 1', 'Group 2', 'Group 3']
-
-fig = ff.create_distplot(
-        hist_data, group_labels, bin_size=[.1, .25, .5])
-#st.plotly_chart(fig, use_container_width=True)
-#NEWCODE
 
 
 # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
